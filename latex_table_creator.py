69deac15c1994248a6057219c3b629b5073de224
Jordi Ricard Onrubia Palacio <jordirop@gmail.com>
2018-07-28 15:53:49 +0200

    typo fixed

---
+++ latex_table_creator.py
@@ -0,0 +1,14 @@
+import argparse
+import pandas
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--header', default='None', type=str, help='header style')
+parser.add_argument('--file', type=str, help='csv/ssv file')
+parser.add_argument('--separator', default= ',' type=str, help='separator')
+
+def main():
+
+if __name__ == '__name__':
+    main()
+    sys.exit(0)
